41fa02f8e81bfa222c3d2be00187b29573311f41
Carst Tankink <ctankink@bol.com>
2025-12-04 07:38:00 +0100

    AoC-day-4 Star 2

--- src/main/kotlin/day4/PrintingDepartment.kt
+++ src/main/kotlin/day4/PrintingDepartment.kt
@@ -1,6 +1,8 @@
 package day4
 
+import util.Grid
 import util.Helpers.Companion.toGrid
+import util.Point
 import util.Solution
 
 enum class Location {
@@ -15,12 +17,33 @@ class PrintingDepartment(fileName: String?) : Solution<List<Location>, Int>(file
 
     override fun solve1(data: List<List<Location>>): Int {
         val map = data.toGrid()
-        return map.entries.count {(pos, content) ->
-            content == Location.PAPER && pos.getNeighbours(cardinal = false).count { map[it] == Location.PAPER } < 4
-        }
+        val accessible = findAccessible(map, emptySet())
+        return accessible.size
     }
 
     override fun solve2(data: List<List<Location>>): Int {
-        TODO("Not yet implemented")
+        val map = data.toGrid()
+
+        tailrec fun removeRolls(removed: Set<Point>): Set<Point> {
+            val accessible = findAccessible(map, removed)
+            return if (accessible.isEmpty()) removed else removeRolls(removed + accessible)
+        }
+
+        return removeRolls(emptySet()).size
+    }
+
+
+    private fun findAccessible(map: Grid<Location>, removed: Set<Point>): Set<Point> {
+        return map.entries
+            .filter { (pos, content) -> pos !in removed && content == Location.PAPER }
+            .filter { (pos, _) ->
+                val accessiblePositions = pos
+                    .getNeighbours(cardinal = false)
+                    .count { it in removed || (map[it] ?: Location.EMPTY) == Location.EMPTY }
+                accessiblePositions > 4
+            }
+            .map { it.key }
+            .toSet()
+
     }
 }
--- src/test/kotlin/day4/PrintingDepartmentTest.kt
+++ src/test/kotlin/day4/PrintingDepartmentTest.kt
@@ -10,4 +10,9 @@ class PrintingDepartmentTest {
     fun sample1star1() {
         assertEquals(13, sample1.star1())
     }
+
+    @Test
+    fun sample1star2() {
+        assertEquals(43, sample1.star2())
+    }
 }
